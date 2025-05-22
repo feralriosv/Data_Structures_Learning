5e99cacb4224dc19bfee808968f55ed9835547db
Fernando Rios <feralriosvas1011@gmail.com>
2025-05-22 11:41:05 +0200

    Implement a more efficient hash function using folding method

--- src/hashing/MyHashTable.java
+++ src/hashing/MyHashTable.java
@@ -24,8 +24,21 @@ public class MyHashTable<T> implements IMyHashing<T> {
     }
 
     public int hashFunction(Object o) {
-        int elementHash = o.hashCode();
-        return elementHash % size;
+        int hashCode = Math.abs(o.hashCode());
+
+        if (hashCode == Integer.MIN_VALUE) return 0;
+
+        String hashString = Integer.toString(hashCode);
+
+        if (hashString.length() == 1) return hashCode % size;
+
+        return fold(hashString);
+    }
+
+    private int fold(String value) {
+        String first = value.substring(0, value.length() / 2);
+        String second = value.substring(value.length() / 2);
+        return  (Integer.parseInt(first) + Integer.parseInt(second)) % size;
     }
 
     @Override
--- src/hashing/Testing.java
+++ src/hashing/Testing.java
@@ -1,13 +1,15 @@
 package hashing;
 
 import java.util.HashSet;
-import java.util.LinkedList;
 
 public class Testing {
     public static void main(String[] args) {
         MyHashTable<String> myHashTable = new MyHashTable<>(12);
-        HashSet<String> ds = new HashSet<>(1, 12);
         String name = "Fernando";
         System.out.println(myHashTable.hashFunction(name));
+        String second = "7";
+        System.out.println(myHashTable.hashFunction(second));
+        int o1 = 10000000;
+        System.out.println(myHashTable.hashFunction(o1));
     }
 }

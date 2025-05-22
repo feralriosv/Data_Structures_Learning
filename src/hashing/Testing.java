ea7dad9a20a9338db8e4f5c15c017c2ff0d2e7b4
Fernando Rios <feralriosvas1011@gmail.com>
2025-05-22 08:38:01 +0200

    Commit Test class

--- src/hashing/Testing.java
+++ src/hashing/Testing.java
@@ -1,11 +1,13 @@
 package hashing;
 
 import java.util.HashSet;
+import java.util.LinkedList;
 
 public class Testing {
     public static void main(String[] args) {
         MyHashTable<String> myHashTable = new MyHashTable<>(12);
-        HashSet<String> ds = new HashSet<>(-1, 12);
-        System.out.println(myHashTable.applyHash("Fernando"));
+        HashSet<String> ds = new HashSet<>(1, 12);
+        String name = "Fernando";
+        System.out.println(myHashTable.hashFunction(name));
     }
 }

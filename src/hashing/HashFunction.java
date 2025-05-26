4a869a3df7c1581e4e6c6a86f92de8e5fb2c34d0
Fernando Rios <feralriosvas1011@gmail.com>
2025-05-26 21:49:10 +0200

    Add new HashFunction class to separate function logic

---
+++ src/hashing/HashFunction.java
@@ -0,0 +1,27 @@
+package hashing;
+
+
+public class HashFunction<K, V> {
+    private final MyHashTable<K, V> table;
+
+    public HashFunction(MyHashTable<K, V> table) {
+        this.table = table;
+    }
+
+    public int apply(K key) {
+        int hashCode = Math.abs(key.hashCode());
+        if (hashCode == Integer.MIN_VALUE) return 0;
+
+        String hashString = Integer.toString(hashCode);
+        if (hashString.length() == 1) return hashCode % table.getCapacity();
+
+        return fold(hashString);
+    }
+
+    private int fold(String value) {
+        String first = value.substring(0, value.length() / 2);
+        String second = value.substring(value.length() / 2);
+        return  (Integer.parseInt(first) + Integer.parseInt(second)) % table.getCapacity();
+    }
+
+}

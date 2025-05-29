60591c743c5b30bb8626c19554c12423cdea6461
Fernando Rios <feralriosvas1011@gmail.com>
2025-05-29 07:14:37 +0200

    Implement first class draft

--- src/linear/fifo/MyPriorityQueue.java
+++ src/linear/fifo/MyPriorityQueue.java
@@ -1,4 +1,30 @@
 package linear.fifo;
 
-public class MyProprityQueue {
+import java.util.Collection;
+
+public class MyPriorityQueue<E> implements IMyQueue<E> {
+
+    private E[] data;
+    private int size;
+
+    public MyPriorityQueue(int capacity) {
+        //noinspection unchecked
+        data = (E[]) new Object[capacity];
+        size = 0;
+    }
+
+    @Override
+    public boolean enqueue(E e) {
+        return false;
+    }
+
+    @Override
+    public boolean dequeue() {
+        return false;
+    }
+
+    @Override
+    public void remove() {
+
+    }
 }

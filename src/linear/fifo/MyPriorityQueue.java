88fca1e042f3aef45cf35c99ca1f4ef64823dd7c
Fernando Rios <feralriosvas1011@gmail.com>
2025-05-29 08:46:04 +0200

    Implement core methods excepting remove and dequeue

--- src/linear/fifo/MyPriorityQueue.java
+++ src/linear/fifo/MyPriorityQueue.java
@@ -1,21 +1,55 @@
 package linear.fifo;
 
-import java.util.Collection;
+public class MyPriorityQueue<E extends Comparable<E>> implements IMyQueue<E> {
 
-public class MyPriorityQueue<E> implements IMyQueue<E> {
-
-    private E[] data;
+    private final E[] data;
     private int size;
+    private final int capacity;
 
     public MyPriorityQueue(int capacity) {
+        this.capacity = capacity;
         //noinspection unchecked
-        data = (E[]) new Object[capacity];
+        data = (E[]) new Comparable[capacity];
         size = 0;
     }
 
     @Override
     public boolean enqueue(E e) {
+        if (capacity == size) {
+            return false;
+        }
+
+        int index = size - 1;
+
+        while (index >= 0 && e.compareTo(data[index]) < 0) {
+            data[index + 1] = data[index];
+            index--;
+        }
+
+//        Re-establishes the index
+        data[index + 1] = e;
+        size++;
         return false;
+    }
+
+    public boolean isEmpty() {
+        return size == 0;
+    }
+
+    public String toString() {
+        StringBuilder s = new StringBuilder();
+        if (isEmpty()) {
+            return "[]";
+        }
+        s.append("[");
+        for (int i = 0; i < size; i++) {
+            s.append(data[i]);
+            if (i < size - 1) {
+                s.append(", ");
+            }
+        }
+        s.append("]");
+        return s.toString();
     }
 
     @Override

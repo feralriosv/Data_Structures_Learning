b3d58a6c5e00cb42d5d85172e39c5dad88a15fcb
Fernando Rios <feralriosvas1011@gmail.com>
2025-06-21 16:27:49 +0200

    Implement countInversion methods

--- src/algorithms/AlgorithmProblems.java
+++ src/algorithms/AlgorithmProblems.java
@@ -1,22 +1,25 @@
 package algorithms;
 
-import java.beans.PropertyEditorManager;
 import java.util.*;
 
 public class AlgorithmProblems {
     public static void main(String[] args) {
         int[] arr1 = {1, 2, 4, 5, 5, 5, 5, 4, 54, 23, 42, 43};
         int[] arr2 = {};
-        int[] arr3 = {-1, -1, -1, 0};
+        int[] arr3 = {-1, -1, -1, 0 , -1};
         int[] arr4 = {5};
 
         hasFrequentElementOverK(null, 1);
         hasFrequentElementOverK(arr1, 3);
         hasFrequentElementOverK(arr2, 0);
         hasFrequentElementOverK(arr3, 2);
-        System.out.println("-----");
+        System.out.print(System.lineSeparator());
         hasPairWithSum(arr1, -1);
         hasPairWithSum(arr4, 5);
+        System.out.print(System.lineSeparator());
+        countInversionsBrute(arr1);
+        countInversionsBrute(arr2);
+          countInversionsOptimal(arr3);
     }
 
     /**
@@ -27,7 +30,6 @@ public class AlgorithmProblems {
      * @param k the repetition threshold
      */
     public static void hasFrequentElementOverK(int[] array, int k) {
-
         if (array == null || array.length < 1 || k < 0) {
             System.err.println("Invalid arguments");
             return;
@@ -52,7 +54,6 @@ public class AlgorithmProblems {
         } else {
             System.out.println("No match");
         }
-
     }
 
     /**
@@ -63,7 +64,6 @@ public class AlgorithmProblems {
      * @param k the target sum
      */
     public static void hasPairWithSum(int[] array, int k) {
-
         if (array == null || array.length < 2) {
             System.err.println("Invalid array");
             return;
@@ -75,16 +75,109 @@ public class AlgorithmProblems {
             int complement = k - number;
 
             if (numbers.contains(complement)) {
-                System.out.println("Pair found: " + number + " + " + complement + " = " + k);
+                System.out.printf("Pair found: %d + %d = %d%n", number, complement, k);
                 return;
             }
-
             numbers.add(number);
         }
 
         System.out.println("No pair found");
-
     }
 
+    /**
+     * Calculates the number of unordered index pairs (i, j) in the array
+     * such that i < j and A[i] > A[j].
+     * This value is known as the "disorder" or inversion count of the array.
+     * <p>
+     * Runtime: O(n^2)
+     *
+     * @param array the array of integers (can contain duplicates)
+     */
+    public static void countInversionsBrute(int[] array) {
+        if (array == null) {
+            System.err.println("Invalid array");
+            return;
+        }
 
+        int inversionCounter = 0;
+        int n = array.length;
+
+        for (int i = 0; i < n; i++) {
+            for (int j = 0; j < n; j++) {
+                if (i < j && array[i] > array[j]) {
+                    inversionCounter++;
+                }
+            }
+        }
+
+        System.out.println("Total inversions: " + inversionCounter);
+    }
+
+    /**
+     * Counts the number of inversions in the given array using a modified MergeSort algorithm.
+     * An inversion is a pair of indices (i, j) such that i < j and A[i] > A[j].
+     * <p>
+     * Runtime: O(n·log(n))
+     *
+     * @param array the input array of integers
+     */
+    public static void countInversionsOptimal(int[] array) {
+        if (array == null) {
+            System.err.println("Invalid array");
+            return;
+        }
+
+        int totalInversions = sortAndCountInversions(array, 0, array.length - 1);
+        System.out.println(totalInversions);
+    }
+
+    private static int mergeAndCount(int[] array, int low, int mid, int high) {
+        int lowPartLength = mid - low + 1;
+        int highPartLength = high - mid;
+
+        int[] lowPart = Arrays.copyOfRange(array, low, mid + 1);
+        int[] highPart = Arrays.copyOfRange(array, mid + 1, high + 1);
+
+        int indexLow = 0, indexHigh = 0, inversionCounter = 0;
+        int mergeIndex = low;
+
+        while (indexLow < lowPartLength && indexHigh < highPartLength) {
+
+            if (lowPart[indexLow] <= highPart[indexHigh]) {
+                array[mergeIndex] = lowPart[indexLow];
+                indexLow++;
+            } else {
+                array[mergeIndex] = highPart[indexHigh];
+                indexHigh++;
+                inversionCounter += lowPartLength - indexLow;
+            }
+
+            mergeIndex++;
+        }
+
+        copyIfRest(lowPart, array, indexLow, mergeIndex);
+        copyIfRest(highPart, array, indexHigh, mergeIndex);
+
+        return inversionCounter;
+    }
+
+    private static int sortAndCountInversions(int[] array, int low, int high) {
+        int partialInversions = 0;
+
+        if (low < high) {
+            int mid = (low + high) / 2;
+
+            partialInversions += sortAndCountInversions(array, low, mid);
+            partialInversions += sortAndCountInversions(array, mid + 1, high);
+            partialInversions += mergeAndCount(array, low, mid, high);
+        }
+
+        return partialInversions;
+    }
+
+    private static void copyIfRest(int[] source, int[] target, int sourceIndex, int targetIndex) {
+        while (sourceIndex < source.length) {
+            target[targetIndex++] = source[sourceIndex++];
+        }
+    }
 }

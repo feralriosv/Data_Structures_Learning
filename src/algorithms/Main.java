92d6d3ccfcd44ad93bdebae5873657a30c3cdf37
Fernando Rios <feralriosvas1011@gmail.com>
2025-06-24 00:06:33 +0200

    Separate input handling and output messaged from logic

--- src/algorithms/Main.java
+++ src/algorithms/Main.java
@@ -3,7 +3,18 @@ package algorithms;
 import java.util.Arrays;
 import java.util.List;
 
-public class Testing {
+public class Main {
+    private static final String ELEMENT_INDEX_FOUND_MESSAGE = "Element found at index %d%n";
+    private static final String ELEMENT_INDEX_NOT_FOUND_MESSAGE = "Element not found, it should be introduced at index %d";
+    private static final String TOTAL_INVERSIONS_MESSAGE = "Total inversions found: %d%n";
+    private static final String MORE_THAN_K_TIMES_FOUND_MESSAGE = "Match with maximal frequency %d at value %d%n";
+    private static final String SUM_PAIR_FOUND_MESSAGE = "Pair found: %d + %d = %d%n";
+
+    private static final String MORE_THAN_K_TIMES_NOT_FOUND_MESSAGE = "No value has frequency > %d";
+    private static final String SUM_PAIR_NOT_FOUND_MESSAGE = "No pair found";
+
+    private static final String ERROR_INVALID_ARGUMENTS = "Invalid arguments";
+
     public static void main(String[] args) {
         int[] arr1 = {1, 2, 4, 5, 5, 5, 5, 4, 54, 23, 42, 43};
         int[] arr2 = {};
@@ -12,18 +23,80 @@ public class Testing {
         int[] arr5 = {7, 8, 5, 4, 5, 2};
         List<Integer> list1 = List.of(3, 4, 6, 8, 14, 34);
 
-        Algorithmic.hasFrequentElementOverK(null, 1);
-        Algorithmic.hasFrequentElementOverK(arr1, 3);
-        Algorithmic.hasFrequentElementOverK(arr2, 0);
-        Algorithmic.hasFrequentElementOverK(arr3, 2);
+        isElementMoreThanKTimes(null, 1);
+        isElementMoreThanKTimes(arr1, 3);
+        isElementMoreThanKTimes(arr2, 0);
+        isElementMoreThanKTimes(arr3, 2);
+        getElementIndex(list1, 8);
         System.out.print(System.lineSeparator());
-        Algorithmic.hasPairWithSum(arr1, -1);
-        Algorithmic.hasPairWithSum(arr4, 5);
+        findPairWithSum(arr1, -1);
+        findPairWithSum(arr4, 5);
         System.out.print(System.lineSeparator());
-        Algorithmic.countInversionsBrute(arr1);
-        Algorithmic.countInversionsBrute(arr2);
-        Algorithmic.countInversionsOptimal(arr3);
+        countInversionsBrute(arr1);
+        countInversionsBrute(arr2);
+        countInversionsOptimal(arr3);
+    }
 
-        System.out.println(Algorithmic.getIndexBinarySearch(list1, 11));
+    // Private methods that validate user inputs separately from algorithm logic:
+
+    private static void countInversionsOptimal(int[] array) {
+        if (array == null) {
+            System.err.println(ERROR_INVALID_ARGUMENTS);
+            return;
+        }
+
+        int totalInversions = Algorithmic.sortAndCountInversions(array, 0, array.length - 1);
+        System.out.printf(TOTAL_INVERSIONS_MESSAGE, totalInversions);
+    }
+
+    private static void countInversionsBrute(int[] array) {
+        if (array == null) {
+            System.err.println(ERROR_INVALID_ARGUMENTS);
+            return;
+        }
+
+        int totalInversions = Algorithmic.countInversionsBrute(array);
+        System.out.printf(TOTAL_INVERSIONS_MESSAGE, totalInversions);
+    }
+
+    private static void isElementMoreThanKTimes(int[] array, int k) {
+        if (array == null || array.length < 1 || k < 0) {
+            System.err.println(ERROR_INVALID_ARGUMENTS);
+            return;
+        }
+
+        int[] result = Algorithmic.isMoreThanKTimes(array, k);
+
+        if (result != null) {
+            System.out.printf((MORE_THAN_K_TIMES_FOUND_MESSAGE), result[0], result[1]);
+        } else {
+            System.out.printf(MORE_THAN_K_TIMES_NOT_FOUND_MESSAGE, k);
+        }
+    }
+
+    private static void findPairWithSum(int[] array, int sum) {
+        if (array == null || array.length < 2) {
+            System.err.println(ERROR_INVALID_ARGUMENTS);
+            return;
+        }
+
+        int[] pairs = Algorithmic.findPairWithSum(array, sum);
+
+        if (pairs != null) {
+            System.out.printf((SUM_PAIR_FOUND_MESSAGE), pairs[0], pairs[1], sum);
+        } else {
+            System.out.println(SUM_PAIR_NOT_FOUND_MESSAGE);
+        }
+    }
+
+    private static void getElementIndex(List<Integer> list, int element) {
+        int index = Algorithmic.indexBinarySearch(list, element);
+
+        if (index > 0) {
+            System.out.printf(ELEMENT_INDEX_FOUND_MESSAGE, index);
+        } else {
+            index = -1 * index;
+            System.out.printf(ELEMENT_INDEX_NOT_FOUND_MESSAGE, index);
+        }
     }
 }
